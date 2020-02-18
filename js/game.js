3e9f0c4484524ad12b75bd5a03dd17166c56987b
eliburgi <elias.burgstaller@gmx.at>
2020-02-18 10:44:47 +0100

    setup basic Pixi application

---
+++ js/game.js
@@ -0,0 +1,22 @@
+// the colors used in the app
+const kBackgroundColor = 0x000028;
+
+// create a PIXI application
+let app = new PIXI.Application({
+    width: 256,
+    height: 256,
+    antialias: true,
+    resolution: 1
+});
+
+// add the canvas that Pixi automatically created to the HTML page
+document.body.appendChild(app.view);
+
+// use a nice background color for the canvas
+app.renderer.backgroundColor = kBackgroundColor;
+
+// make the canvas fill the entire window
+app.renderer.view.style.position = "absolute";
+app.renderer.view.style.display = "block";
+app.renderer.autoResize = true;
+app.renderer.resize(window.innerWidth, window.innerHeight);
