eec228aa4449c030ea13630ea441800623f93939
eliburgi <elias.burgstaller@gmx.at>
2020-02-19 15:43:47 +0100

    creating dots with random colors

--- js/teleporting-dots.js
+++ js/teleporting-dots.js
@@ -1,7 +1,7 @@
 // canvas background color
 const kBackgroundColor = 0x000028;
 // number of dots that will be created
-const kInitialDotCount = 1;
+const kInitialDotCount = 30;
 
 // the PIXI application
 var app;
@@ -16,19 +16,14 @@ class Dot {
         this.y = 0;
         this.radius = 10;
         this.color = 0xffffff;
-
-        this.t = 0;
     }
 
     update(dt) {
-        console.log(dt);
-        this.t += dt; 
-        this.x = Math.sin(this.t / 100) * 50;
     }
 
     render(graphics) {
         graphics.beginFill(this.color);
-        graphics.drawRect(this.x, 30, 50, 50);
+        graphics.drawCircle(this.x, this.y, this.radius);
     }
 }
 
@@ -41,10 +36,13 @@ function createInitialDots() {
 }
 
 function createRandomDot() {
+    let width = canvasWidth();
+    let height = canvasHeight();
     let newDot = new Dot();
-
-    // TODO: set properties
-
+    newDot.radius = width * 4 / 640;
+    newDot.x = random(0, width);
+    newDot.y = random(0, height);
+    newDot.color = randomColor();
     return newDot;
 }
 
@@ -85,4 +83,61 @@ app.ticker.add(dt => gameLoop(dt));
 function gameLoop(dt) {
     dotsGraphics.clear();
     updateDots(dt);
+}
+
+function canvasWidth() {
+    return window.innerWidth;
+}
+
+function canvasHeight() {
+    return window.innerHeight;
+}
+
+function random(inclusive, exclusive) {
+    return inclusive + Math.random() * (exclusive - inclusive);
+}
+
+function randomColor() {
+    let hue = random(0, 1);
+    let saturation = 0.5;
+    let value = 1.0;
+
+    let rgb = HSVtoRGB(hue, saturation, value);
+    let hexStr = rgbToHex(rgb.r, rgb.g, rgb.b);
+    return PIXI.utils.string2hex(hexStr);
+}
+
+// Source: https://stackoverflow.com/questions/17242144/javascript-convert-hsb-hsv-color-to-rgb-accurately
+function HSVtoRGB(h, s, v) {
+    var r, g, b, i, f, p, q, t;
+    if (arguments.length === 1) {
+        s = h.s, v = h.v, h = h.h;
+    }
+    i = Math.floor(h * 6);
+    f = h * 6 - i;
+    p = v * (1 - s);
+    q = v * (1 - f * s);
+    t = v * (1 - (1 - f) * s);
+    switch (i % 6) {
+        case 0: r = v, g = t, b = p; break;
+        case 1: r = q, g = v, b = p; break;
+        case 2: r = p, g = v, b = t; break;
+        case 3: r = p, g = q, b = v; break;
+        case 4: r = t, g = p, b = v; break;
+        case 5: r = v, g = p, b = q; break;
+    }
+    return {
+        r: Math.round(r * 255),
+        g: Math.round(g * 255),
+        b: Math.round(b * 255)
+    };
+}
+
+// Source: https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+function rgbToHex(r, g, b) {
+    function componentToHex(c) {
+        var hex = c.toString(16);
+        return hex.length == 1 ? "0" + hex : hex;
+    }
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
 }
