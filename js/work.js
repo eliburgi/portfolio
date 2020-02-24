69d07e4aa62757c651d90204a14732108c78a128
eliburgi <elias.burgstaller@gmx.at>
2020-02-24 10:30:17 +0100

    card overlay now displays icon buttons for github, play store and app store

--- js/work.js
+++ js/work.js
@@ -4,22 +4,30 @@ const myWork = [
     {
         title: 'sQrz',
         desc: 'A highly addictive puzzle-game.',
-        img: 'img/work/sQrz.png'
+        img: 'img/work/sQrz.png',
+        githubUrl: 'TODO',
+        playStoreUrl: 'TODO',
+        appStoreUrl: 'TODO'
     },
     {
         title: 'Celery With Me',
         desc: 'Inspired by @medicalmedium. For every celery lover out there.',
         img: 'img/work/celery_with_me.png',
-        fontFamily: 'Pacifico',
+        githubUrl: 'TODO',
+        playStoreUrl: 'TODO',
+        fontFamily: 'Pacifico'
     },
     {
         title: 'sQrz',
-        img: 'img/work/sQrz.png'
+        img: 'img/work/sQrz.png',
+        githubUrl: 'TODO',
+        appStoreUrl: 'TODO'
     },
     {
         title: 'Celery With Me',
         fontFamily: 'Pacifico',
-        img: 'img/work/celery_with_me.png'
+        img: 'img/work/celery_with_me.png',
+        githubUrl: 'TODO'
     },
     {
         title: 'sQrz',
@@ -91,8 +99,38 @@ function _buildWorkCard(work) {
 
     var overlay = document.createElement('div');
     overlay.setAttribute('class', 'card-work-overlay');
-    overlay.innerHTML = 'Hallo';
     card.appendChild(overlay);
+
+    var overlayContent = document.createElement('div');
+    overlayContent.setAttribute('class', 'overlay-content');
+    overlay.appendChild(overlayContent);
+
+    if (work.githubUrl) {
+        var githubButton = document.createElement('button');
+        githubButton.setAttribute('class', 'icon-button');
+        overlayContent.appendChild(githubButton);
+        var githubIcon = document.createElement('i');
+        githubIcon.setAttribute('class', 'fa fa-github fa-3x');
+        githubButton.appendChild(githubIcon);
+    }
+
+    if (work.playStoreUrl) {
+        var googlePlayButton = document.createElement('button');
+        googlePlayButton.setAttribute('class', 'icon-button');
+        overlayContent.appendChild(googlePlayButton);
+        var googlePlayIcon = document.createElement('i');
+        googlePlayIcon.setAttribute('class', 'fa fa-android fa-3x');
+        googlePlayButton.appendChild(googlePlayIcon);
+    }
+
+    if (work.appStoreUrl) {
+        var appStoreButton = document.createElement('button');
+        appStoreButton.setAttribute('class', 'icon-button');
+        overlayContent.appendChild(appStoreButton);
+        var appStoreIcon = document.createElement('i');
+        appStoreIcon.setAttribute('class', 'fa fa-apple fa-3x');
+        appStoreButton.appendChild(appStoreIcon);
+    }
 
     return card;
 }
