32aeac8d7f5eceeb79ea5b02a3b7723e8006d9fd
eliburgi <elias.burgstaller@gmx.at>
2020-02-28 06:43:00 +0100

    app screens carousel works (without animation)

--- js/header.js
+++ js/header.js
@@ -1,14 +1,40 @@
 
 const kAppScreens = [
     'img/header/app.png',
-    'img/header/app.png',
-    'img/header/app.png'
+    'img/header/app-1.png',
+    'img/header/app-2.png'
 ];
 
-let leftScreenImg = document.getElementById('left-app-screen');
-let frontScreenImg = document.getElementById('front-app-screen');
-let rightScreenImg = document.getElementById('right-app-screen');
+var _frontIndex = 0;
+var _leftIndex = () => _frontIndex - 1 >= 0 ? _frontIndex - 1 : kAppScreens.length - 1;
+var _rightIndex = () => _frontIndex + 1 < kAppScreens.length ? _frontIndex + 1 : 0;
 
-leftScreenImg.setAttribute('src', kAppScreens[0]);
-frontScreenImg.setAttribute('src', kAppScreens[1]);
-rightScreenImg.setAttribute('src', kAppScreens[2]);
+var leftScreenImg = document.getElementById('left-app-screen');
+var frontScreenImg = document.getElementById('front-app-screen');
+var rightScreenImg = document.getElementById('right-app-screen');
+
+leftScreenImg.addEventListener('click', (e) => rotateRight());
+rightScreenImg.addEventListener('click', (e) => rotateLeft());
+
+_updateScreens();
+
+function rotateRight() {
+    _frontIndex = _leftIndex();
+
+    /* TODO: animate change */ 
+    _updateScreens();
+}
+
+function rotateLeft() {
+    _frontIndex = _rightIndex();
+
+    /* TODO: animate change */ 
+    _updateScreens();
+}
+
+function _updateScreens() {
+    leftScreenImg.setAttribute('src', kAppScreens[_leftIndex()]);
+    frontScreenImg.setAttribute('src', kAppScreens[_frontIndex]);
+    rightScreenImg.setAttribute('src', kAppScreens[_rightIndex()]);
+}
+
