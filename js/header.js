aee628a7baed9ae81790c7bf84559c1ae3daa4d9
eliburgi <elias.burgstaller@gmx.at>
2020-02-29 18:11:50 +0100

    app gallery - left rotation animation works

--- js/header.js
+++ js/header.js
@@ -1,8 +1,8 @@
 
 const kAppScreens = [
     'img/header/app.png',
-    'img/header/app-1.png',
-    'img/header/app-2.png'
+    'img/header/app.png',
+    'img/header/app.png'
 ];
 
 var _frontIndex = 0;
@@ -12,24 +12,48 @@ var _rightIndex = () => _frontIndex + 1 < kAppScreens.length ? _frontIndex + 1 :
 var leftScreenImg = document.getElementById('left-app-screen');
 var frontScreenImg = document.getElementById('front-app-screen');
 var rightScreenImg = document.getElementById('right-app-screen');
+var tmpScreenImg = document.getElementById('tmp-app-screen');
 
-leftScreenImg.addEventListener('click', (e) => rotateRight());
-rightScreenImg.addEventListener('click', (e) => rotateLeft());
+leftScreenImg.addEventListener('click', (e) => _rotateRight());
+rightScreenImg.addEventListener('click', (e) => _rotateLeft());
 
 _updateScreens();
 
-function rotateRight() {
+function _rotateRight() {
     _frontIndex = _leftIndex();
 
     /* TODO: animate change */ 
-    _updateScreens();
+    // front -> right
+    // left -> front
+    // right -> left and gets new img
+    // _updateScreens();
 }
 
-function rotateLeft() {
+function _rotateLeft() {
     _frontIndex = _rightIndex();
 
-    /* TODO: animate change */ 
-    _updateScreens();
+    tmpScreenImg.setAttribute('src', kAppScreens[_leftIndex()]);
+    leftScreenImg.setAttribute('src', kAppScreens[_leftIndex()]);
+    frontScreenImg.setAttribute('src', kAppScreens[_frontIndex]);
+    rightScreenImg.setAttribute('src', kAppScreens[_rightIndex()]);
+
+    /* hack to restart animation everytime this function is called */ 
+    /* source: https://css-tricks.com/restart-css-animation/ */ 
+    tmpScreenImg.classList.remove('left-disappear');
+    void tmpScreenImg.offsetWidth;
+    tmpScreenImg.classList.add('left-disappear');
+
+    leftScreenImg.classList.remove('front-rotate-left');
+    void leftScreenImg.offsetWidth;
+    leftScreenImg.classList.add('front-rotate-left');
+
+    frontScreenImg.classList.remove('right-rotate-left');
+    void frontScreenImg.offsetWidth;
+    frontScreenImg.classList.add('right-rotate-left');
+
+    rightScreenImg.classList.remove('right-appear');
+    void rightScreenImg.offsetWidth;
+    rightScreenImg.classList.add('right-appear');
 }
 
 function _updateScreens() {
